0fa475d5ee157166d8895844582ef1ac105ef0ba
Raphael Lelis <raphalelis@outlookc.com>
2022-05-18 18:31:48 -0300

    create app react

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App-header">
+		<div className="container">
+			<p>Quantidade</p>
+			<hr/>
+			
+		</div>
+    </div>
+  )
+}
+
+export default App
